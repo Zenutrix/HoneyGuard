e48224242a5c2180fb83dd76b4b9131f2d26c094
Thomas Schoepf <thomas@schoepf-tirol.at>
2023-06-09 20:13:29 +0200

    modified:   Honeyguard.py

--- Honeyguard.py
+++ Honeyguard.py
@@ -92,20 +92,19 @@ def initialize_gpio(button_pin, led_pin):
     GPIO.setup(led_pin, GPIO.OUT)
     return GPIO
 
-def read_weight(hx711, scale_ratio, tare):
+def read_weight(hx711):
     if hx711:
         try:
             measures = hx711.get_raw_data(times=5)
             weight = sum(measures) / len(measures) if measures else None
             if weight is not None:
-                # Perform scaling calculations to convert raw value to kilograms
-                weight_kg = (weight - tare) / scale_ratio
-
+                # Perform scaling calculations here using the 'weight' value
+                # ...
                 weight_json = {
                     "measurement": "weight",
                     "time": int(time.time() * 10**9),
                     "fields": {
-                        "value": weight_kg
+                        "value": weight
                     }
                 }
                 client.write_points([weight_json])
